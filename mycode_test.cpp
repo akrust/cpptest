6537bbe4c03f0d5ec285e1af8baa8decd6ef0123
akrust2 <60387513+akrust2@users.noreply.github.com>
2020-01-28 13:54:41 +0100

    Update mycode_test.cpp

--- mycode_test.cpp
+++ mycode_test.cpp
@@ -1,4 +1,5 @@
 #include "mycode.h"
+#include <cassert>
 
 // This program should test the code contained in MyClass.
 // It should return 0 if the test succeeds, 1 if the test fails. 
@@ -6,7 +7,7 @@ int main(){
 
   MyClass::isInE("");
   
-  
+  assert(false);
 
   return 0;
 }
